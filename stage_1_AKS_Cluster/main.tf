0276de0bf1a96439d2990bdd29cff0ae90fa0987
Weber <christian.a.weber@partner.bmwgroup.com>
2021-10-09 11:41:26 +0200

    tmp comment

--- stage_1_AKS_Cluster/main.tf
+++ stage_1_AKS_Cluster/main.tf
@@ -12,7 +12,7 @@ terraform {
     resource_group_name  = "meta"
     storage_account_name = "hackathonterraform"
     container_name       = "tfstate"
-    key                  = "***CHANGEME****.tfstate"
+    key                  = "chriscloud27.tfstate"
   }
 }
 
@@ -32,55 +32,54 @@ locals {
 }
 
 # Managed Kubernetes Cluster
-resource "azurerm_resource_group" "global" {
-  name     = local.name
-  location = local.location
-}
+# resource "azurerm_resource_group" "global" {
+#   name     = local.name
+#   location = local.location
+# }
 
-resource "azurerm_kubernetes_cluster" "aks" {
-  name                = local.name
-  resource_group_name = azurerm_resource_group.global.name
-  location            = local.location
-  dns_prefix          = local.name
-  node_resource_group = format("%s-%s", azurerm_resource_group.global.name, "aks-rg")
+# resource "azurerm_kubernetes_cluster" "aks" {
+#   name                = local.name
+#   resource_group_name = azurerm_resource_group.global.name
+#   location            = local.location
+#   dns_prefix          = local.name
+#   node_resource_group = format("%s-%s", azurerm_resource_group.global.name, "aks-rg")
 
-  default_node_pool {
-    name       = "default"
-    node_count = 2
-    vm_size    = "Standard_D2s_v3"
-  }
+#   default_node_pool {
+#     name       = "default"
+#     node_count = 2
+#     vm_size    = "Standard_D2s_v3"
+#   }
 
+#   identity {
+#     type = "SystemAssigned"
+#   }
+# }
 
-  identity {
-    type = "SystemAssigned"
-  }
-}
+# # Reference to a externally created Container Registry
+# data "azurerm_container_registry" "acr" {
+#   name                = "2021hackathon"
+#   resource_group_name = "meta"
+# }
 
-# Reference to a externally created Container Registry
-data "azurerm_container_registry" "acr" {
-  name                = "2021hackathon"
-  resource_group_name = "meta"
-}
+# # Registry Pull permission for the AKS Cluster
+# resource "azurerm_role_assignment" "acr" {
+#   scope                = data.azurerm_container_registry.acr.id
+#   role_definition_name = "AcrPull"
+#   principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity.0.object_id
+# }
 
-# Registry Pull permission for the AKS Cluster
-resource "azurerm_role_assignment" "acr" {
-  scope                = data.azurerm_container_registry.acr.id
-  role_definition_name = "AcrPull"
-  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity.0.object_id
-}
+# #outputs
+# output "resource_group_name" {
+#   value = azurerm_resource_group.global.name
+# }
 
-#outputs
-output "resource_group_name" {
-  value = azurerm_resource_group.global.name
-}
+# output "aks_name" {
+#   value = azurerm_kubernetes_cluster.aks.name
+# }
 
-output "aks_name" {
-  value = azurerm_kubernetes_cluster.aks.name
-}
-
-output "acr_name" {
-  value = data.azurerm_container_registry.acr.name
-}
-output "acr_url" {
-  value = data.azurerm_container_registry.acr.login_server
-}
+# output "acr_name" {
+#   value = data.azurerm_container_registry.acr.name
+# }
+# output "acr_url" {
+#   value = data.azurerm_container_registry.acr.login_server
+# }
